ed7d128c346bd164caf9b997c0cdb165c2126ff8
kennaloughrey <166356352+kennaloughrey@users.noreply.github.com>
2024-04-25 16:41:54 -0600

    Fourth Project update

--- MyFourthProject/App copy 4.js
+++ MyFourthProject/App copy 4.js
@@ -1,55 +1,39 @@
 import React from 'react';
-import { Text, Image, ScrollView} from 'react-native';
+import { Text, View, FlatList} from 'react-native';
 
-const dog = {
-  uri: 'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/dog2.png',
-  width: 64,
-  height: 64
-};
+export default StatesApp = () => {
+  return (
+    <View style={{flex: 1, paddingTop:22}}>
+        <FlatList
+          data={[
+            {key: 'Alabama'},
+            {key: 'Alaska'},
+            {key: 'Arizona'},
+            {key: 'Arkansas'},
+            {key: 'California'},
+            {key: 'Colorado'},
+            {key: 'Delaware'},
+            {key: 'Flordia'},
+            {key: 'Georgia'},
+            {key: 'Hawaii'},
+            {key: 'Idaho'},
+            {key: 'Illinois'},
+            {key: 'Indiana'},
+            {key: 'Iowa'},
+            {key: 'Kansas'},
+            {key: 'Kentucky'},
+            {key: 'Maine'},
+            {key: 'Maryland'},
+            {key: 'Massachusetts'},
+            {key: 'Mnnesota'},
+            {key: 'Mississippi'}
 
-export default MyScrollViewApp = () => (
+          ]}
+          renderItem={({item}) => <Text style={{padding: 10, fontSize: 20, height: 44,}}> {item.key} </Text>}
+      />
 
-        <ScrollView style={{padding: 40}}>
-        <Text style={{fontSize: 80}}> Try to scroll down </Text>
-        <Image source={require('./assets/dog2.png')} style={{width: 80, height: 80}} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Text style={{fontSize: 80}}> Try to scroll down again, if you like </Text>
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Text style={{fontSize: 80}}> Try to scroll down </Text>
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Text style={{fontSize: 80}}> Try to scroll down </Text>
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Text style={{fontSize: 80}}> React Native Components! </Text>
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-        <Image source={dog} />
-      </ScrollView>
-
-);
+     </View>
+     
+  );// end of return statement
+ }
+      
