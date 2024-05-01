eb7c73c034b5b64fa36bd4251a1b389b536d1d00
kennaloughrey <166356352+kennaloughrey@users.noreply.github.com>
2024-05-01 14:09:33 -0600

    Seventh Project

--- MySeventhProject/App.js
+++ MySeventhProject/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react'; 
+import { StyleSheet, Text, View, Button } from 'react-native'; 
 
-export default function App() {
+export default class TouchableButton extends Component {
+
+  onPressButton(){
+    alert('You tapped a button')
+  }
+
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+
+      <View style={styles.containerButton}> 
+        <Button 
+            onPress={this.onPressButton}
+            title="Press Button 1" 
+      /> 
+      </View> 
+
+
+      <View style={this.styles.containerLayoutButton}> 
+
+      <Button  
+          onPress={this.onPressButton}
+            title="Press Button 2" 
+      /> 
+
+  
+      <Button  
+          onPress={this.onPressButton}
+            title="Great!" 
+            color='green' 
+      />
+
+      </View> 
+
+  
+
+      <View style={styles.containerButton}> 
+
+      <Button  
+          onPress={() => { 
+            alert('You tapped button Tap Me!'); 
+          }} 
+            title="Tap Me!" 
+      /> 
+
+      </View>
+
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}}
